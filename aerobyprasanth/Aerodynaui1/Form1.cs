421b25f5105ba9d7a4fa851a7359f6db82897a8c
I am Prasanth <prasanthelectronics01@gmail.com>
2024-10-12 02:20:51 +0530

    Add files via upload
    Ui 1 updated

---
+++ aerobyprasanth/Aerodynaui1/Form1.cs
@@ -0,0 +1,15 @@
+namespace newaero
+{
+    public partial class adminmanager : Form
+    {
+        public adminmanager()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ aerobyprasanth/Aerodynaui1/employeeportal.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace newaero
+{
+    partial class employeeportal
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(employeeportal));
+            button3 = new Button();
+            label9 = new Label();
+            label6 = new Label();
+            linkLabel1 = new LinkLabel();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            textBox6 = new TextBox();
+            label2 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            richTextBox1 = new RichTextBox();
+            textBox2 = new TextBox();
+            label10 = new Label();
+            dateTimePicker2 = new DateTimePicker();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.Location = new Point(428, 622);
+            button3.Name = "button3";
+            button3.Size = new Size(161, 34);
+            button3.TabIndex = 100;
+            button3.Text = "Download file";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Georgia", 13F);
+            label9.Location = new Point(170, 548);
+            label9.Name = "label9";
+            label9.Size = new Size(116, 30);
+            label9.TabIndex = 93;
+            label9.Text = "Deadline";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Georgia", 13F);
+            label6.Location = new Point(1105, 356);
+            label6.Name = "label6";
+            label6.Size = new Size(85, 30);
+            label6.TabIndex = 92;
+            label6.Text = "Status";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.ActiveLinkColor = Color.Black;
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = SystemColors.MenuHighlight;
+            linkLabel1.Font = new Font("Segoe UI", 12F);
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(1769, 146);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(105, 32);
+            linkLabel1.TabIndex = 89;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "LOGOUT";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.MenuHighlight;
+            label3.Font = new Font("Georgia", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(34, 146);
+            label3.Name = "label3";
+            label3.Size = new Size(300, 35);
+            label3.TabIndex = 88;
+            label3.Text = "Employee Dashboard";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.MenuHighlight;
+            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.Location = new Point(3, 118);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1911, 85);
+            pictureBox1.TabIndex = 86;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Berlin Sans FB", 20F);
+            label1.Location = new Point(34, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(414, 44);
+            label1.TabIndex = 87;
+            label1.Text = "AEROTECH DYNAMICS";
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 10F);
+            textBox6.Location = new Point(428, 287);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(588, 34);
+            textBox6.TabIndex = 107;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Georgia", 13F);
+            label2.Location = new Point(170, 426);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 30);
+            label2.TabIndex = 106;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Georgia", 13F);
+            label7.Location = new Point(170, 286);
+            label7.Name = "label7";
+            label7.Size = new Size(102, 30);
+            label7.TabIndex = 104;
+            label7.Text = "Task ID";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Georgia", 13F);
+            label8.Location = new Point(166, 356);
+            label8.Name = "label8";
+            label8.Size = new Size(208, 30);
+            label8.TabIndex = 110;
+            label8.Text = "Task Description";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(428, 547);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(588, 31);
+            dateTimePicker1.TabIndex = 111;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(1344, 358);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(125, 29);
+            radioButton1.TabIndex = 112;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Completed";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(1524, 357);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(122, 29);
+            radioButton2.TabIndex = 113;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Inprogress";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(1699, 356);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(103, 29);
+            radioButton3.TabIndex = 114;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Rejected";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(428, 357);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(588, 145);
+            richTextBox1.TabIndex = 98;
+            richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 10F);
+            textBox2.Location = new Point(1342, 289);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(522, 34);
+            textBox2.TabIndex = 115;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Georgia", 13F);
+            label10.Location = new Point(1105, 289);
+            label10.Name = "label10";
+            label10.Size = new Size(102, 30);
+            label10.TabIndex = 116;
+            label10.Text = "Task ID";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(1342, 426);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(522, 31);
+            dateTimePicker2.TabIndex = 117;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Georgia", 13F);
+            label4.Location = new Point(1105, 426);
+            label4.Name = "label4";
+            label4.Size = new Size(208, 30);
+            label4.TabIndex = 118;
+            label4.Text = "Completion Date";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1344, 490);
+            button1.Name = "button1";
+            button1.Size = new Size(161, 34);
+            button1.TabIndex = 119;
+            button1.Text = "Upload file";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1568, 490);
+            button2.Name = "button2";
+            button2.Size = new Size(161, 34);
+            button2.TabIndex = 99;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // employeeportal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(label10);
+            Controls.Add(textBox2);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label8);
+            Controls.Add(textBox6);
+            Controls.Add(label2);
+            Controls.Add(label7);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(richTextBox1);
+            Controls.Add(label9);
+            Controls.Add(label6);
+            Controls.Add(linkLabel1);
+            Controls.Add(label3);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "employeeportal";
+            Size = new Size(1920, 1080);
+            Load += employeeportal_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button button3;
+        private Label label9;
+        private Label label6;
+        private LinkLabel linkLabel1;
+        private Label label3;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox textBox6;
+        private Label label2;
+        private Label label7;
+        private Label label8;
+        private DateTimePicker dateTimePicker1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RichTextBox richTextBox1;
+        private TextBox textBox2;
+        private Label label10;
+        private DateTimePicker dateTimePicker2;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+    }
+}
